157ec5ea27ef58f2249cdb5d629afd9bc8594569
Cugurel <cugurel7@gmail.com>
2025-04-24 20:55:27 +0300

    update

---
+++ UI/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UI.Controllers
+{
+	public class UserController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult UserDetail()
+		{
+			return View();
+		}
+	}
+}
