e2733f07a32b05bf15d2599a2ed333e5f7f41441
Cugurel <cugurel7@gmail.com>
2025-04-30 21:20:05 +0300

    project update

---
+++ Entity/Concrete/Product.cs
@@ -0,0 +1,26 @@
+﻿using Entity.Abstract;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Concrete
+{
+	public class Product:IEntity
+	{
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public int? QuantityInStock { get; set; }
+        public int? Price { get; set; }
+        public int? CategoryId { get; set; }
+        public string? ImagePath { get; set; }
+        [NotMapped]
+        public IFormFile File { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public bool? IsActive { get; set; }
+    }
+}
---
+++ UI/Controllers/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using DataAccess.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UI.Controllers
+{
+	public class ProductController : Controller
+	{
+		Context c = new Context();
+		public IActionResult Index()
+		{
+			var value = c.Products.ToList();
+			return View(value);
+		}
+	}
+}
