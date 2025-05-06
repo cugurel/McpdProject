5122ade08f96042ba8d656240499d96fc8d4f967
Cugurel <cugurel7@gmail.com>
2025-05-06 20:37:54 +0300

    product

--- UI/Controllers/ProductController.cs
+++ UI/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using DataAccess.Concrete;
+using Entity.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace UI.Controllers
@@ -10,6 +11,29 @@ namespace UI.Controllers
 		{
 			var value = c.Products.ToList();
 			return View(value);
+		}
+
+		public IActionResult DeleteProduct(int Id)
+		{
+			//Linq ile id kullanarak veri çekme
+			var value = c.Products.Find(Id);
+			c.Products.Remove(value);
+			c.SaveChanges();
+			return RedirectToAction("Index", "Product");
+		}
+
+		[HttpGet]
+		public IActionResult AddProduct()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult AddProduct(Product product)
+		{
+			c.Products.Add(product);
+			c.SaveChanges();
+			return RedirectToAction("Index", "Product");
 		}
 	}
 }
