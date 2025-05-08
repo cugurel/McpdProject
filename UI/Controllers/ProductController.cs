3d8dfa035e375ac455b7f2151bc6f0d6e8127644
Cugurel <cugurel7@gmail.com>
2025-05-08 20:57:25 +0300

    update product

--- UI/Controllers/ProductController.cs
+++ UI/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using DataAccess.Concrete;
 using Entity.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace UI.Controllers
 {
@@ -25,15 +26,57 @@ namespace UI.Controllers
 		[HttpGet]
 		public IActionResult AddProduct()
 		{
+			ViewBag.Categories = c.Categories.Select(u => new SelectListItem
+			{
+				Value = u.Id.ToString(),
+				Text = u.Name
+			}).ToList();
+
 			return View();
 		}
 
 		[HttpPost]
 		public IActionResult AddProduct(Product product)
 		{
+			ViewBag.Categories = c.Categories.Select(u => new SelectListItem
+			{
+				Value = u.Id.ToString(),
+				Text = u.Name
+			}).ToList();
+
 			product.CreatedDate = DateTime.Now;
 			product.IsActive = true;
 			c.Products.Add(product);
+			c.SaveChanges();
+			return RedirectToAction("Index", "Product");
+		}
+
+
+		[HttpGet]
+		public IActionResult UpdateProduct(int Id)
+		{
+			ViewBag.Categories = c.Categories.Select(u => new SelectListItem
+			{
+				Value = u.Id.ToString(),
+				Text = u.Name
+			}).ToList();
+
+			var value = c.Products.Find(Id);
+			return View(value);
+		}
+
+		[HttpPost]
+		public IActionResult UpdateProduct(Product product)
+		{
+			ViewBag.Categories = c.Categories.Select(u => new SelectListItem
+			{
+				Value = u.Id.ToString(),
+				Text = u.Name
+			}).ToList();
+
+			product.CreatedDate = DateTime.Now;
+			product.IsActive = true;
+			c.Products.Update(product);
 			c.SaveChanges();
 			return RedirectToAction("Index", "Product");
 		}
