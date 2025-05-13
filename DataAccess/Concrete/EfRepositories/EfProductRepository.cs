960bd9dcc1dd8660b5e0ca75bb49bb21a6e4ae34
Cugurel <cugurel7@gmail.com>
2025-05-13 21:04:21 +0300

    efproduct repository

--- DataAccess/Concrete/EfRepositories/EfProductRepository.cs
+++ DataAccess/Concrete/EfRepositories/EfProductRepository.cs
@@ -1,14 +1,41 @@
 ﻿using DataAccess.Abstract;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Entity.Concrete;
 
 namespace DataAccess.Concrete.EfRepositories
 {
 	public class EfProductRepository : IProductDal
 	{
+		public void Add(Product product)
+		{
+			using var c = new Context();
+			c.Products.Add(product);
+			c.SaveChanges();
+		}
 
+		public void Delete(Product product)
+		{
+			using var c = new Context();
+			c.Products.Remove(product);
+			c.SaveChanges();
+		}
+
+		public List<Product> GetAll()
+		{
+			using var c = new Context();
+			return c.Products.ToList();
+		}
+
+		public Product GetById(int Id)
+		{
+			using var c = new Context();
+			return c.Products.Find(Id);
+		}
+
+		public void Update(Product product)
+		{
+			using var c = new Context();
+			c.Products.Update(product);
+			c.SaveChanges();
+		}
 	}
 }
