05e1532b5221686465f71836273cb61fdcde4b10
Cugurel <cugurel7@gmail.com>
2025-05-13 20:44:36 +0300

    update

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,18 @@
+﻿using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+	public interface IProductDal
+	{
+		void Add(Product product);
+		void Update(Product product);
+		void Delete(Product product);
+		Product GetById(int Id);
+		List<Product> GetAll();
+	}
+}
