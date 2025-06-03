f74c76d4088016d62b0c0675c5ca449a8f0555ff
Cugurel <cugurel7@gmail.com>
2025-06-03 19:51:49 +0300

    Identty

---
+++ UI/Models/Identity/RegisterModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UI.Models.Identity
+{
+	public class RegisterModel
+	{
+        [Required]
+        public string Firstname { get; set; }
+		[Required]
+		public string Surname { get; set; }
+		[Required]
+		public string Phone { get; set; }
+		[Required]
+		public string Email { get; set; }
+		[Required]
+		public string Password { get; set; }
+	}
+}
--- UI/Program.cs
+++ UI/Program.cs
@@ -1,7 +1,33 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using UI.Models.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationContext>(options =>
+{
+	options.UseSqlServer("server = Cagri; database=CrmDb; " +
+		"integrated security=true; TrustServerCertificate = True",
+		builder => builder.EnableRetryOnFailure());
+});
+
+builder.Services.AddSession(options =>
+{
+	options.IdleTimeout = TimeSpan.FromMinutes(60);
+});
+
+builder.Services.AddIdentity<User, IdentityRole>().
+	AddEntityFrameworkStores<ApplicationContext>().AddDefaultTokenProviders();
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+	options.LoginPath = "/Auth/Login";
+	options.LogoutPath = "/Auth/Logout";
+	options.SlidingExpiration = true;
+	options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+});
 
 var app = builder.Build();
 
@@ -13,9 +39,10 @@ if (!app.Environment.IsDevelopment())
 	app.UseHsts();
 }
 
+app.UseSession();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+app.UseAuthentication();
 app.UseRouting();
 
 app.UseAuthorization();
