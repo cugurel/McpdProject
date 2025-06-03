e2a17b47450826690bd5f4f9543b4dd2e8a37579
Cugurel <cugurel7@gmail.com>
2025-06-03 21:07:21 +0300

    auth register

---
+++ UI/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UI.Models.Identity;
+
+namespace UI.Controllers
+{
+	public class AuthController : Controller
+	{
+		[HttpGet]
+		public IActionResult Register()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Register(RegisterModel model)
+		{
+			return View();
+		}
+	}
+}
